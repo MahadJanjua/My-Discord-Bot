faa33526e8091cf41a5f7ca51ccdd55b73c99276
Bob Feng <bobfengli@gmail.com>
2020-06-23 14:18:17 -0400

    Adding case sensitivity to hello command

--- source.py
+++ source.py
@@ -3,9 +3,13 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext.commands import Bot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+BOT_PREFIX = ("!", "?")
+client = Bot(command_prefix=BOT_PREFIX)
 
 class CustomClient(discord.Client):
     async def on_ready(self):
@@ -14,20 +18,26 @@ class CustomClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
             return
-        if message.content == '$hello' or message.content == '$mallo':
-            response = str(message.content).replace("$", "") + str(message.author).split("#", 1)[0] + "!"
+        if message.content.lower() == '$hello' or message.content == '$mallo':
+            response = str(message.content).replace("$", "") + " " + str(message.author).split("#", 1)[0] + "!"
             await message.channel.send(response)
         elif str(message.content).lower() == 'mwo':
             await message.channel.send('mwo')
     
+    # @client.command(name = '8ball',
+    #             description = "Answers a yes/no question.",
+    #             brief = "Ask yes/no question.",
+    #             aliases=['eight_ball', 'eightball', '8-ball'],
+    #             pass_context=True)
     # async def eight_ball(self, context):
     #     possible_responses1 = [
     #         'That is a resounding no',
-    #         'It does not looking likely',
+    #         'It does not look likely',
     #         'Too hard to tell',
     #         'It is quite possible',
     #         'Definitely',
     #         'Ask Mahad',
+    #         'MWO?',
     #     ]
     #     await client.say(random.choice(possible_responses1) + ", " + context.message.author.mention)
 
