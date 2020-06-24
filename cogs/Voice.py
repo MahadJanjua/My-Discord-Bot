1ade22085f7e3d9291f3e6c43170839acdc42f46
unknown <mahad.j15@hotmail.com>
2020-06-24 13:03:05 -0400

    Added mp3 functionality to voice

--- cogs/Voice.py
+++ cogs/Voice.py
@@ -11,16 +11,31 @@ class Voice(commands.Cog):
 
     @commands.command(pass_context=True)
     async def join(self, ctx):
-        vc = await ctx.message.author.voice.channel.connect()
+        global voice
+        channel = ctx.message.author.voice.channel
+        voice = self.client.voice_clients
+
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else: voice = await channel.connect()
+        await ctx.send(f"Joined {channel}")
+        
     
-    @commands.command(
-        name = 'leave',
-        pass_context=True
-    )
+    @commands.command(name = 'leave', pass_context=True)
     async def destroy(self, ctx):
-        for x in self.client.voice_clients:
-            if(x.guild == ctx.message.guild):
-                return await x.disconnect()
+        channel = ctx.message.author.voice.channel
+        if voice and voice.is_connected():
+            await voice.disconnect()
+            await ctx.send(f"Left {channel}")
+    
+    @commands.command(name = 'mwo', pass_context=True)
+    async def mwo(self, ctx):
+        channel = ctx.message.author.voice.channel
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(executable="extra/ffmpeg.exe", source="extra/mwo.mp3"))
+        ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+
+
 
 def setup(client):
     client.add_cog(Voice(client))
